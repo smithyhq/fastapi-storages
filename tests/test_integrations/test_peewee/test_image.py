4e12763f025bbfacf4fc520500f6eaeac3d13a3b
Amin Alaee <amin.alaee.zaviyeh@postnl.nl>
2026-03-31 12:20:43 +0200

    update

--- tests/test_integrations/test_peewee/test_image.py
+++ tests/test_integrations/test_peewee/test_image.py
@@ -14,7 +14,7 @@ from tests.test_integrations.utils import UploadFile
 db = SqliteDatabase(database_name)
 
 
-class Model(Model):
+class TestModel(Model):
     id = AutoField(primary_key=True)
     image = ImageType(storage=FileSystemStorage(path="/tmp"), null=True)
 
@@ -24,24 +24,24 @@ class Model(Model):
 
 @pytest.fixture(autouse=True)
 def prepare_database():
-    db.create_tables([Model])
+    db.create_tables([TestModel])
     yield
-    db.drop_tables([Model])
+    db.drop_tables([TestModel])
 
 
 def test_valid_image(tmp_path: Path) -> None:
-    Model.image.storage = FileSystemStorage(path=str(tmp_path))
+    TestModel.image.storage = FileSystemStorage(path=str(tmp_path))
 
     input_file = tmp_path / "input.png"
     image = Image.new("RGB", (800, 1280), (255, 255, 255))
     image.save(input_file, "PNG")
 
     upload_file = UploadFile(file=input_file.open("rb"), filename="image.png")
-    Model.create(image=upload_file)
-    model = Model.get()
+    TestModel.create(image=upload_file)
+    model = TestModel.get()
 
     assert model.image.name == "image.png"
-    assert model.image.size == 5847
+    assert model.image.size == input_file.stat().st_size
     assert model.image.path == str(tmp_path / "image.png")
 
 
@@ -51,19 +51,19 @@ def test_invalid_image(tmp_path: Path) -> None:
     upload_file = UploadFile(file=input_file.open("rb"), filename="image.png")
 
     with pytest.raises(ValidationException):
-        Model.create(image=upload_file)
+        TestModel.create(image=upload_file)
 
 
 def test_nullable_image() -> None:
-    Model.create(image=None)
-    model = Model.get()
+    TestModel.create(image=None)
+    model = TestModel.get()
 
     assert model.image is None
 
 
 def test_clear_empty_image() -> None:
     upload_file = UploadFile(file=io.BytesIO(b""), filename="")
-    Model.create(image=upload_file)
-    model = Model.get()
+    TestModel.create(image=upload_file)
+    model = TestModel.get()
 
     assert model.image is None
